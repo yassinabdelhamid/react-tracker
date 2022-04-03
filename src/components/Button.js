582c1465c225516f6b3343d879ca20cbba60ae61
Yassin Abdelhamid <yassin1001@gmail.com>
2022-04-03 16:27:51 +0200

    Added test event for button onclick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-  return <button style={{backgroundColor: color}} className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            style={{ backgroundColor: color }}
+            className='btn'>{text}
+        </button>
+    )
 }
 
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,16 @@ import Button from './Button'
 
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='add' />
+        <Button 
+        color='green' 
+        text='add'
+        onClick={onClick} />
     </header>
   )
 }
