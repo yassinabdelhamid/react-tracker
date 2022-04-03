feb7bd8296f9e27a9026ba4cff78ec70a3f0e8cc
Yassin Abdelhamid <yassin1001@gmail.com>
2022-04-03 16:31:58 +0200

    Added task components

---
+++ src/components/Task.js
@@ -0,0 +1,30 @@
+const task = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Dinner',
+        day: 'Feb 5th at 6:30pm',
+        reminder: false,
+    }
+]
+
+const Task = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Task
